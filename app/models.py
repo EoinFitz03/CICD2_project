83f4a559ed2b8201300c3eb0166d80eef5eaa951
EoinFitz03 <118372036+EoinFitz03@users.noreply.github.com>
2025-11-19 10:47:52 +0000

    Dcoker

--- app/models.py
+++ app/models.py
@@ -13,15 +13,11 @@ class Base(DeclarativeBase):
 class UserDB(Base):
     __tablename__ = "users"
 
-    # Primary key
+  
     user_id: Mapped[int] = mapped_column(primary_key=True, index=True)
-
-    # Basic fields
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     age: Mapped[int] = mapped_column(Integer, nullable=False)
-
-    # Enum stored as DB enum
     gender: Mapped[GenderEnum] = mapped_column(
         SAEnum(GenderEnum, name="gender_enum"),
         nullable=False,
